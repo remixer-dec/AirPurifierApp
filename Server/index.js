ed8ee27719d049281605a49c99afa0b04332964b
remixer-dec <remixer.dec@gmail.com>
2021-06-12 14:44:58 +0300

    improved formatting

--- Server/index.js
+++ Server/index.js
@@ -2,34 +2,41 @@ var cfg = require('./config')
 var miio = require('miio');
 var ws = require("nodejs-websocket")
 
-miio.device({address: cfg.ip, token: cfg.token})
-.then(device => {console.log('Connected to', device); initServer(device)})
-.catch(err => console.error(err));
+miio.device({
+        address: cfg.ip,
+        token: cfg.token
+    })
+    .then(device => {
+        console.log('Connected to', device);
+        initServer(device)
+    })
+    .catch(err => console.error(err));
 
 var device = false
-function initServer(d){
+
+function initServer(d) {
     device = d
-    var server = ws.createServer(function (conn) {
-        conn.on('error', function (err) {
+    var server = ws.createServer(function(conn) {
+        conn.on('error', function(err) {
             if (err.code !== 'ECONNRESET') {
                 // Ignore ECONNRESET and re throw anything else
                 throw err
             }
         })
-        conn.on("text", function (str) {
-               console.log("Received "+str)
-               parseCommand(str).then(reply=>{
-                   if(reply){
-                       conn.sendText(reply)
-                   }
-               })
+        conn.on("text", function(str) {
+            console.log("Received " + str)
+            parseCommand(str).then(reply => {
+                if (reply) {
+                    conn.sendText(reply)
+                }
+            })
         })
-    }).listen(cfg.serverport,cfg.serverhost)
+    }).listen(cfg.serverport, cfg.serverhost)
 }
 
-async function parseCommand(txt){
+async function parseCommand(txt) {
     cmd = txt.split(':')
-    switch(cmd[0]){
+    switch (cmd[0]) {
         case "on":
             device.turnOn()
         break;
--- Server/miio-fix.js
+++ Server/miio-fix.js
@@ -3,5 +3,5 @@ const fs = require('fs')
 let filename = './node_modules/miio/lib/safeishJSON.js'
 file = fs.readFileSync(filename).toString('utf-8')
 file = file.replace('str =',"str = str.replace(',,', ',');\n str =")
-fs.writeFileSync(filename,file)
+fs.writeFileSync(filename, file)
 console.log('FIXED')
